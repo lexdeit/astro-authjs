1125605d3f7f8d96d9fe02a04bf4b520858ad255
LEXDEIT <godhood@proton.me>
2024-12-29 00:56:43 -0600

    feat: producs and images schema

--- db/config.ts
+++ db/config.ts
@@ -20,10 +20,37 @@ const Role = defineTable({
   }
 });
 
+const Product = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    stock: column.number(),
+    slug: column.text({ unique: true }),
+    price: column.number(),
+    sizes: column.text(),
+    tags: column.text(),
+    title: column.text(),
+    description: column.text(),
+    gender: column.text(),
+    type: column.text(),
+
+    user: column.text({ references: () => User.columns.id })
+  }
+})
+
+const ProductImage = defineTable({
+  columns: {
+    id: column.text({ primaryKey: true }),
+    productId: column.text({ references: () => Product.columns.id }),
+    image: column.text()
+  }
+});
+
 // https://astro.build/db/config
 export default defineDb({
   tables: {
     Role,
-    User
+    User,
+    Product,
+    ProductImage,
   }
 });
