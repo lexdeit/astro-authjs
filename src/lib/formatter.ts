ea96972aaad4c29925a5e36d4eece1a0670d320d
LEXDEIT <godhood@proton.me>
2024-12-30 16:02:14 -0600

    fix: minor changes

---
+++ src/lib/formatter.ts
@@ -0,0 +1,10 @@
+export class Formatter {
+    static currency(value: number, decimals = 2): string {
+        return new Intl.NumberFormat('en-US', {
+            style: "currency",
+            currency: "usd",
+            maximumFractionDigits: decimals
+        }).format(value);
+
+    }
+}
