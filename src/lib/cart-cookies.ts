02ecef3c7c07d99c09b69151d5e3991e8b238d0e
LEXDEIT <godhood@proton.me>
2024-12-30 14:22:41 -0600

    feat: added cart logic

---
+++ src/lib/cart-cookies.ts
@@ -0,0 +1,54 @@
+import type { CartItem } from "@/interfaces";
+import Cookies from "js-cookie";
+
+
+
+export class CartCookiesClient {
+
+
+    static getCart(): CartItem[] {
+
+
+        const cart = JSON.parse(Cookies.get("cart") || "[]");
+
+        return cart;
+    }
+
+
+    static addItem(cartItem: CartItem): CartItem[] {
+
+
+
+        const cart = CartCookiesClient.getCart();
+
+        const existingItem = cart.find(item => item.productId === cartItem.productId && item.size === cartItem.size);
+
+        if (existingItem) {
+            existingItem.quantity += cartItem.quantity;
+        } else {
+            cart.push(cartItem);
+        }
+
+
+        Cookies.set('cart', JSON.stringify(cart));
+
+
+        return cart;
+    }
+
+
+    static removeItem(productId: string, size: string): CartItem[] {
+
+
+        const cart = CartCookiesClient.getCart();
+
+        const updatedCart = cart.filter(item => !(item.productId === productId && item.size === size));
+
+
+        Cookies.set('cart', JSON.stringify(cart));
+
+
+        return updatedCart;
+    }
+
+}
