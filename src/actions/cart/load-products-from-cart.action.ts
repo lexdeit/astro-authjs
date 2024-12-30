991436fb60bbae35b6b0dd100ec8614fb820d5ef
LEXDEIT <godhood@proton.me>
2024-12-30 16:03:11 -0600

    fix: returns data

--- src/actions/cart/load-products-from-cart.action.ts
+++ src/actions/cart/load-products-from-cart.action.ts
@@ -1,19 +1,45 @@
 import type { CartItem } from "@/interfaces";
 import { defineAction } from "astro:actions";
-import { z } from "astro:schema";
+import { db, eq, inArray, Product, ProductImage } from "astro:db";
 
 
 export const loadProductsFromCart = defineAction({
     accept: "json",
     // input: z.string(),
-    handler: async (_, { cookies }) => {
+    handler: async (_, ctx) => {
 
+        const { cookies } = ctx;
 
         const cart = JSON.parse(cookies.get("cart")?.value ?? '[]') as CartItem[];
+        if (cart.length === 0) return [];
+
+        const productIds = cart.map(product => product.productId);
+
+        const dbProducts = await db.select().from(Product).innerJoin(ProductImage, eq(Product.id, ProductImage.productId)).where(inArray(Product.id, productIds));
+
+        const items = cart.map(item => {
 
 
+            const dbProduct = dbProducts.find(p => p.Product.id === item.productId);
 
+            if (!dbProduct) {
+                throw new Error(`Product not found with id ${item.productId}`);
+            }
 
-        return;
+            const { title, price, slug } = dbProduct.Product;
+            const image = dbProduct.ProductImage.image;
+
+            return {
+                productId: item.productId,
+                title: title,
+                size: item.size,
+                quantity: Number(item.quantity),
+                image: image.startsWith('http') ? image : `${import.meta.env.PUBLIC_URL}/images/products/${image}`,
+                price: price,
+                slug: slug
+            }
+        });
+
+        return items;
     }
 });
